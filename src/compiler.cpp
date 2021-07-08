746e433d274703cd50cfcfe56bd96f0f01fd908e
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-07-08 20:16:52 +0200

    Add debug logs for script origin

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,24 +1,25 @@
 #include "compiler.h"
+#include <iostream>
 
 extern BytecodeCompiler::Bytecode BytecodeCompiler::CompileSourceIntoBytecode(v8::Isolate* isolate, const char* name, const char* sourceCode)
 {
     v8::Isolate::Scope isolateScope(isolate);
     v8::HandleScope scope(isolate);
 
-    v8::ScriptOrigin origin(
-        isolate,
-        v8::String::NewFromUtf8(isolate, name).ToLocalChecked(),
-        0,
-        0,
-        false,
-        -1,
-        v8::Local<v8::Value>(),
-        false,
-        false,
-        true,
-        v8::Local<v8::PrimitiveArray>()
-    );
-    v8::ScriptCompiler::Source source(v8::String::NewFromUtf8(isolate, sourceCode).ToLocalChecked(), origin);
+    v8::ScriptOrigin scriptOrigin(
+		isolate,
+		v8::String::NewFromUtf8(isolate, name).ToLocalChecked(),
+		0,
+		0, false,
+		-1,
+		v8::Local<v8::Value>(),
+		false,
+		false,
+		true,
+		v8::Local<v8::PrimitiveArray>()
+	);
+    std::cout << scriptOrigin.Options().IsModule() << std::endl;
+    v8::ScriptCompiler::Source source(v8::String::NewFromUtf8(isolate, sourceCode).ToLocalChecked(), scriptOrigin);
     auto maybeModule = v8::ScriptCompiler::CompileModule(isolate, &source);
     auto module = maybeModule.ToLocalChecked();
     auto unboundScript = module->GetUnboundModuleScript();
