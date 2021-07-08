6892c3768c5cee0712fec1ed957c82a20aa246d5
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-07-08 19:24:51 +0200

    Fix GetVersion not being found

--- include/compiler.h
+++ include/compiler.h
@@ -3,13 +3,15 @@
 
 namespace BytecodeCompiler 
 {
+    extern uint32_t GetVersion();
+
     struct Bytecode
     {
         uint8_t* bytecode;
         int size;
         uint32_t version;
 
-        Bytecode(v8::ScriptCompiler::CachedData* data) : size(data->length), version(GetVersion()) 
+        Bytecode(v8::ScriptCompiler::CachedData* data) : size(data->length), version(BytecodeCompiler::GetVersion()) 
         {
             bytecode = new uint8_t[data->length];
             memcpy(bytecode, data->data, data->length);
@@ -17,6 +19,5 @@ namespace BytecodeCompiler
         ~Bytecode() { delete bytecode; }
     };
 
-    extern uint32_t GetVersion();
     extern Bytecode CompileSourceIntoBytecode(v8::Isolate* isolate, v8::ScriptOrigin& origin, const char* sourceCode);
 }
