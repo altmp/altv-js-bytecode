d6bf2bee13a9aab5956a1f47a6f570829b4db8c0
LeonMrBonnie <leonmrbonnie@hotmail.com>
2021-12-15 19:12:51 +0100

    Remove old compiler

--- src/util/compiler.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "v8.h"
-
-namespace BytecodeCompiler 
-{
-    v8::ScriptCompiler::CachedData* CompileSourceIntoBytecode(v8::Isolate* isolate, const char* name, const char* sourceCode);
-};
