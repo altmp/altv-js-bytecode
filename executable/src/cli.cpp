72972c3a343036997c540b6eb8412d1d43f87486
LeonMrBonnie <leonmrbonnie@hotmail.com>
2022-01-17 23:58:13 +0100

    Early continue

--- executable/src/cli.cpp
+++ executable/src/cli.cpp
@@ -11,13 +11,12 @@ Parser::Parser(int argc, char* argv[])
     for(size_t i = 0; i < argc; i++)
     {
         std::string arg = argv[i];
-        if(arg.find("--") == 0)
-        {
-            std::string name = arg.substr(2);
-            if(i + 1 < argc) args[name] = argv[i + 1];
-            else
-                args[name] = "";
-            i++;
-        }
+        if(arg.find("--") != 0) continue;
+
+        std::string name = arg.substr(2);
+        if(i + 1 < argc) args[name] = argv[i + 1];
+        else
+            args[name] = "";
+        i++;
     }
 }
