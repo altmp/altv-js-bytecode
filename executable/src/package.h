9e92008f941c811132248b9e451c67997e3f2f8b
LeonMrBonnie <leonmrbonnie@hotmail.com>
2022-01-28 18:34:10 +0100

    Create executable package class

---
+++ executable/src/package.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "compiler.h"
+
+#include <filesystem>
+#include <fstream>
+
+namespace fs = std::filesystem;
+
+class Package : public BytecodeCompiler::IPackage
+{
+    fs::path outPath;
+    fs::path inPath;
+
+public:
+    Package(fs::path _outPath, fs::path _inPath) : outPath(_outPath), inPath(_inPath) {}
+
+    bool WriteFile(const std::string& fileName, void* data, size_t size) override
+    {
+        // Remove the input path from the file path
+        size_t result = fileName.find(inPath.string());
+        if(result == std::string::npos) return false;
+        std::string filePath = fileName.substr(result);
+
+        fs::path path = outPath / filePath;
+        std::ofstream file(path, std::ios::out | std::ios::binary);
+        if(!file.good()) return false;
+        file.write((char*)data, size);
+        file.close();
+        return true;
+    }
+    bool ReadFile(const std::string& fileName, void* data, size_t size) override
+    {
+        std::ifstream file(fileName, std::ios::in | std::ios::binary);
+        if(!file.good()) return false;
+        file.read((char*)data, size);
+        file.close();
+        return true;
+    }
+    bool FileExists(const std::string& fileName) override
+    {
+        return fs::exists(fileName);
+    }
+    bool FileExists(const fs::path& path)
+    {
+        return FileExists(path.string());
+    }
+    size_t GetFileSize(const std::string& fileName) override
+    {
+        return fs::file_size(fileName);
+    }
+    std::string ResolveFile(const std::string& file, const std::string& basePath) override
+    {
+        fs::path path = fs::path(basePath) / file;
+        if(!fs::exists(path)) return std::string();
+        fs::path rootPath = path.root_path().string();
+        fs::path fileName = path.filename().string();
+        if(fileName.empty())
+        {
+            if(FileExists(rootPath / "index.js")) fileName = "index.js";
+            else if(FileExists(rootPath / "index.mjs"))
+                fileName = "index.mjs";
+            else
+                return std::string();
+        }
+        else
+        {
+            if(FileExists(rootPath / fs::path(fileName.string() + ".js"))) fileName += ".js";
+            else if(FileExists(rootPath / fs::path(fileName.string() + ".mjs")))
+                fileName += ".mjs";
+            else if(FileExists(rootPath / fs::path(fileName.string() + "/index.js")))
+                fileName += "/index.js";
+            else if(FileExists(rootPath / fs::path(fileName.string() + "/index.mjs")))
+                fileName += "/index.mjs";
+            else if(!FileExists(fileName))
+                return std::string();
+        }
+        return (rootPath / fileName).string();
+    }
+};
