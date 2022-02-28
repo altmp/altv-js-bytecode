b89f40353b44ea333ebddd01ed396f98b8044098
LeonMrBonnie <leonmrbonnie@hotmail.com>
2022-03-01 00:34:28 +0100

    Add jsb-module command

--- module/src/main.cpp
+++ module/src/main.cpp
@@ -1,6 +1,31 @@
 #include "cpp-sdk/SDK.h"
 #include "cpp-sdk/version/version.h"
+#include "Log.h"
 #include "runtime.h"
+
+static void CommandHandler(const std::vector<std::string>& args)
+{
+    if(args.size() == 0)
+    {
+        Log::Colored << "~y~Usage: ~w~jsb-module [options]" << Log::Endl;
+        Log::Colored << "  Use: ~ly~\"jsb-module --help\" ~w~for more info" << Log::Endl;
+    }
+    else if(args[0] == "--version")
+    {
+        Log::Colored << "~ly~cpp-sdk: #" << ALT_SDK_VERSION << Log::Endl;
+        Log::Colored << "~ly~" << u8"Copyright © 2022 altMP team." << Log::Endl;
+
+        Log::Colored << "~ly~v8: " << v8::V8::GetVersion() << Log::Endl;
+        Log::Colored << "~ly~" << u8"Copyright © 2014 The V8 project authors." << Log::Endl;
+    }
+    else if(args[0] == "--help")
+    {
+        Log::Colored << "~y~Usage: ~w~jsb-module [options]" << Log::Endl;
+        Log::Colored << "~y~Options:" << Log::Endl;
+        Log::Colored << "  ~ly~--help    ~w~- this message." << Log::Endl;
+        Log::Colored << "  ~ly~--version ~w~- version info." << Log::Endl;
+    }
+}
 
 EXPORT bool altMain(alt::ICore* core)
 {
@@ -8,6 +33,8 @@ EXPORT bool altMain(alt::ICore* core)
 
     auto& runtime = JSBytecodeRuntime::Instance();
     core->RegisterScriptRuntime("jsb", &runtime);
+
+    core->SubscribeCommand("jsb-module", &CommandHandler);
 
     return true;
 }
